caefdadc1f5448ed9e05b7ddc95bd23a691a56a8
mileswu <miles.wu@wu-home.co.uk>
2010-11-30 23:57:38 +0000

    Added tri parsing to fitter

--- complex.py
+++ complex.py
@@ -109,20 +109,20 @@ def predict_func_matrix(coeff, spacing):
 	predict = numpy.zeros(len(spacing), complex)
 	loop = 0
 	for w in spacing:
-		matrix = numpy.array([
-			[(r_l + r_0*(1 + b_i))/l + i*w, l_i*g/i_0/l],
-			[-i_0*r_0*(2+b_i)/c, 1/t_i + i*w]
-		])
+		#matrix = numpy.array([
+		#	[(r_l + r_0*(1 + b_i))/l + i*w, l_i*g/i_0/l],
+		#	[-i_0*r_0*(2+b_i)/c, 1/t_i + i*w]
+		#])
 		#matrix = numpy.array([
 		#	[(r_l + r_0*(1 + b_i))/l + i*w, l_i*g/i_0/l, 0],
 		#	[-i_0*r_0*(2+b_i)/c, 1/t_i + i*w, g1/c1],
 		#	[0, -g1/c1, i*w]
 		#])
-		#matrix = numpy.array([
-		#	[(r_l + r_0*(1 + b_i))/l + i*w, l_i*g/i_0/l, 0],
-		#	[-i_0*r_0*(2+b_i)/c, g/c -g*l_i/c + i*w, -g/c],
-		#	[0, -g/c1, g/c1 + g1/c1 + i*w]
-		#])
+		matrix = numpy.array([
+			[(r_l + r_0*(1 + b_i))/l + i*w, l_i*g/i_0/l, 0],
+			[-i_0*r_0*(2+b_i)/c, g/c -g*l_i/c + i*w, -g/c],
+			[0, -g/c1, g/c1 + g1/c1 + i*w]
+		])
 		inv = numpy.linalg.inv(matrix)
 		predict[loop] = l/inv[0][0] - r_l - i*w*l
 
@@ -147,21 +147,31 @@ def error_func(coeff, spacing, source):
 	return error
 
 def fit_nist(s, out):
-	args = (s[0], out)
-	coeff = scipy.optimize.fmin_powell(error_func, numpy.array([10.0, 1.0, 15.0, 1000.0, 41.0, 1.0, 1.0]), args)
-	print "Coeffecients: ", coeff
-	print "Error: ", error_func(coeff, s[0], out)
-	predict = predict_func_matrix(coeff, s[0])
+	predict = []
+	coeffs = []
+	errors = []
+	
+	for i in out:
+		args = (s[0], i)
+		#coeff = scipy.optimize.fmin_powell(error_func, numpy.array([10.0, 1.0, 15.0, 1000.0, 41.0]), args)
+		coeff = scipy.optimize.fmin_powell(error_func, numpy.array([10.0, 1.0, 15.0, 1000.0, 41.0, 1.0, 1.0]), args)
+		predict.append( predict_func_matrix(coeff, s[0]) )
+		coeffs.append(coeff)
+		errors.append(error_func(coeff, s[0], i))
+	
+	return predict, coeffs, errors
 
-	a = plot.subplot(311)
-	a.plot(numpy.log10(s[0]), numpy.real(out), 'o')
-	a.plot(numpy.log10(s[0]), numpy.real(predict), 'o')
-	a = plot.subplot(323)
-	a.plot(numpy.log10(s[0]), numpy.imag(out), 'o')
-	a.plot(numpy.log10(s[0]), numpy.imag(predict), 'o')
-	a = plot.subplot(133)
-	a.plot(numpy.real(out), numpy.imag(out), 'o')
-	a.plot(numpy.real(predict), numpy.imag(predict), 'o')
+def plot_fit(original, predictions):
+	titles = ["10% Rn","50% Rn","90% Rn"]
+	for i in original:
+		plot.plot(numpy.real(i), numpy.imag(i), ',')
+	for i in predictions:
+		plot.plot(numpy.real(i), numpy.imag(i), 'k-')
+	plot.axes().set_aspect('equal')
+	plot.xlabel("Re(Z) /mOhms")
+	plot.ylabel("Im(Z) /mOhms")
+	
+	plot.legend(titles, numpoints=100, loc=2, ncol=3, bbox_to_anchor=(-0.0, -0.2))
 	plot.show()
 
 
@@ -203,8 +213,12 @@ for i in m_f:
 	out = transfer_func(s, n, temp)
 	m.append(out)
 
-plot_complexn(s[0], m)
-#fit_nist(s, out)
+#plot_complexn(s[0], m)
+predict, coeffs, errors = fit_nist(s, m)
+plot_fit(m, predict)
+
+print coeffs
+print errors
 
 #Coeffecients:  [  5.98340389e+00   6.23565491e-01   1.61887268e-01   1.52429108e+04
   # 5.61840916e+00   1.75209612e+01   2.07340763e+01]
